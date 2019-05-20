8fecadbcb882af2565409f01687f3e35cf0fd5c3
manibabu karnam <mkarnam@corp.miraclesoft.com>
2019-05-20 20:57:14 +0530

    hongten-sesurity

---
+++ hongten-sesurity/security-model/security-model-ai/src/main/java/com/b510/hongten/security/model/ai/App.java
@@ -0,0 +1,13 @@
+package com.b510.hongten.security.model.ai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
